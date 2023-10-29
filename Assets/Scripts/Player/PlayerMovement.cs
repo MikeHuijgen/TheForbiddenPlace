69066c5cbfcd29986ee5af6ce1947ddff074296b
MikeHuijgen <mikeh162001@hotmail.com>
2023-10-29 19:57:29 +0100

    Made the player look towards the forward movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEditor.Build;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -8,6 +9,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float rotationSpeed;
     
     private Rigidbody _rigidbody;
 
@@ -30,6 +32,11 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         _movement = _playerControls.Movement.Move.ReadValue<Vector2>();
+        if (_movement != Vector2.zero)
+        {
+            var newForward = new Vector3(_movement.x,0,_movement.y);
+            transform.forward = Vector3.Lerp(transform.forward, newForward, rotationSpeed * Time.deltaTime);
+        }
     }
 
     private void FixedUpdate()
