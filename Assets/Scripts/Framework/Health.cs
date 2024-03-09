c479b6129e95c935e736008cde8cb32ccb1b7406
MikeHuijgen <mikeh162001@hotmail.com>
2024-03-09 15:14:08 +0100

    Made a basic healthbar
    The healthbar is connected with the health system. But when you dont use a healthbar the health system still works

--- Assets/Scripts/Framework/Health.cs
+++ Assets/Scripts/Framework/Health.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Serialization;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] private float health;
     [SerializeField] private bool isIndestructible;
+
+    public UnityEvent OnTakeDamage = new UnityEvent();
     public void TakeDamage(float damage)
     {
         health -= damage;
+        OnTakeDamage?.Invoke();
         if (health <= 0 && !isIndestructible)
         {
             Destroy(gameObject);
         }
+    }
+
+    public float GetCurrentHealth()
+    {
+        return health;
     }
 }
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+   [SerializeField] private Health health;
+   [SerializeField] private Slider slider;
+
+   private void Awake()
+   {
+      slider.maxValue = health.GetCurrentHealth();
+      slider.value = health.GetCurrentHealth();
+   }
+
+   public void UpdateHealthBar()
+   {
+      slider.value = health.GetCurrentHealth();
+   }
+}
