1bb00519a11248546da615620fed4707eeda81c2
MikeHuijgen <mikeh162001@hotmail.com>
2024-03-15 19:35:06 +0100

    Heakthbar can now fade

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -8,18 +8,47 @@ public class HealthBar : MonoBehaviour
 {
    [SerializeField] private Health health;
    [SerializeField] private Slider slider;
-
-
-   private Color barColor;
+   
+   [Header("Fade Settings")]
+   [SerializeField] private bool canFade;
+   [SerializeField] private float timeBeforeFading;
+   [SerializeField] private float fadeSpeed = 0.1f;
+   
+   private Image fill;
 
    private void Awake()
    {
       slider.maxValue = health.GetCurrentHealth();
       slider.value = health.GetCurrentHealth();
+      if (!canFade) return;
+      fill = slider.fillRect.GetComponent<Image>();
    }
 
    public void UpdateHealthBar()
    {
+      StopAllCoroutines();
+      
       slider.value = health.GetCurrentHealth();
+
+      if (!canFade) return;
+      var newColor = fill.color;
+      newColor.a = 1;
+      fill.color = newColor;
+      
+      StartCoroutine(FadeBar());
+   }
+
+   private IEnumerator FadeBar()
+   {
+      yield return new WaitForSeconds(timeBeforeFading);
+      var newColor = fill.color;
+
+      for (var i = 1f; i > 0; i -= .2f)
+      {
+         Debug.Log("test");
+         newColor.a = i;
+         fill.color = newColor;
+         yield return new WaitForSeconds(fadeSpeed);
+      }
    }
 }
