8f1563673bc2baf35cb8425f5fad85c8a69af0ed
MikeHuijgen <mikeh162001@hotmail.com>
2024-03-16 14:07:45 +0100

    Fixed the bug
    In some scripts there where some weird using cases

--- Assets/Scripts/Framework/HitChecker.cs
+++ Assets/Scripts/Framework/HitChecker.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class HitChecker : MonoBehaviour
 {
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Mathematics;
-using UnityEditor.Build;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
