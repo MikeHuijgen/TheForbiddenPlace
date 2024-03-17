53111694770b574f2825d707575499930f64feb5
MikeHuijgen <mikeh162001@hotmail.com>
2024-03-17 15:54:30 +0100

    Made a basic portal

---
+++ Assets/Scripts/Portal/PortalSystem.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PortalSystem : MonoBehaviour
+{
+    [SerializeField] private List<string> levels = new List<string>();
+}
